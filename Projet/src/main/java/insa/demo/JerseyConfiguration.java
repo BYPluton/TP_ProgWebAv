5a07fff9403435642eb970bdce8af897303f9074
Nicolas_laptop <iblackxstarz@gmail.com>
2021-03-12 09:48:01 +0100

    Fichiers statiques
    Prise en charge des fichiers statiques par Spring-boot

--- Projet/src/main/java/insa/demo/JerseyConfiguration.java
+++ Projet/src/main/java/insa/demo/JerseyConfiguration.java
@@ -3,7 +3,7 @@ package insa.demo;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-
+import org.glassfish.jersey.servlet.ServletProperties;
 import javax.ws.rs.ApplicationPath;
 
 
@@ -13,5 +13,6 @@ import javax.ws.rs.ApplicationPath;
 
 public class JerseyConfiguration extends ResourceConfig {
     public JerseyConfiguration() {
+        property(ServletProperties.FILTER_FORWARD_ON_404, true);
     }
 }
