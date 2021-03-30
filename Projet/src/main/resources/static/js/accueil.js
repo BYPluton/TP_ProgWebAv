4c2c38d20127fc4f52c2fffa2098b385db6f5bec
Nicolas <iblackxstarz@gmail.com>
2021-03-30 22:16:51 +0200

    Ajout de l'affichage dynamique des animes et des listes

---
+++ Projet/src/main/resources/static/js/accueil.js
@@ -0,0 +1,17 @@
+let $maListe = $("#liste");
+
+$(document).ready(function () {
+    $.get("http://localhost:8080/rest/watchlists", function(listes){
+        listes.forEach( liste => ajouter(liste));
+    })
+    /*à modifier pour le delete
+    $(".btn-danger").click(function(){
+        alert();
+        $(this).parent().parent().parent().remove();
+    })*/
+});
+
+
+function ajouter(liste){
+    $maListe.append('<div class="col"><div class="row g-0 border rounded overflow-hidden flex-md-row shadow-sm h-md-250 position-relative"><div class="col p-4 d-flex flex-column position-static"><strong class="d-inline-block mb-2 montexte">Liste n°1</strong><h3 class="mb-0">'+ liste.name +'</h3><p class="card-text mb-auto" style="margin-top: 2.5%;margin-bottom: 2.5%!important;">Liste contenant les animes que je dois voir.</p><div class="btn-group"><button type="button" style="box-sizing: content-box;" class="btn btn-danger">Supprimer</button></div></div> </div></div>');
+}
--- Projet/src/main/resources/static/js/animes.js
+++ Projet/src/main/resources/static/js/animes.js
@@ -1,14 +1,20 @@
-/*$("#monbouton").click(function(){
-    $("#liste").append("<div class=\"col\"><div class=\"card shadow-sm\"><img class =\"card-img-top\" src=\"../static/css/img/anime_db/naruto.jpeg\"><div class=\"card-body\" ><div class=\"d-flex justify-content-between align-items-center\"><p class=\"card-text\">Naruto</p> <small class=\"text-muted\">220 épisodes</small></div></div><div class=\"d-flex justify-content-end align-items-center\"><div class=\"btn-group\"> <button type=\"button\" class=\"btn btn-danger\">Supprimer</button></div></div></div></div>");
-})
+let $maListe = $("#liste");
 
-$(".btn-danger").click(function(){
-    alert("ça marche pas")
-    $(this).parent().parent().parent().remove();
-})
 $(document).ready(function () {
-    
-});*/
-$.get("http://localhost:8080/rest/items", function(data){
-    alert("Data: " + data);
-})
+    $.get("http://localhost:8080/rest/items", function(animes){
+        animes.forEach( anime => ajouter(anime));
+    })
+    /*à modifier pour le delete
+    $(".btn-danger").click(function(){
+        alert();
+        $(this).parent().parent().parent().remove();
+    })*/
+});
+
+alert(session.user.pseudo)
+
+
+
+function ajouter(anime){
+    $maListe.append('<div class="col"><div class="card shadow-sm"><img class="card-img-top" src="'+ anime.url + '"><div class="card-body"><div class="d-flex justify-content-between align-items-center"><p class="card-text">' + anime.titre + '</p><small class="text-muted">' + anime.nbEpisodes +' épisodes</small></div></div><div class="d-flex justify-content-end align-items-center"><div class="btn-group"><button type="button" class="btn btn-danger">Supprimer</button></div></div></div></div>');
+}
